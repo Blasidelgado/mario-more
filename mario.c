bac35e6091575ce23e7ba0485b6fa8a28e562ed8
Blasidelgado <blasidelgado@gmail.com>
2023-06-29 20:50:18 -0300

    cs50x solution

---
+++ mario.c
@@ -0,0 +1,44 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Ask user to input piramid's height
+    int height;
+    do
+    {
+        height = get_int("Enter piramid's height: ");
+    }
+    while (height < 1 || height > 8);
+
+    // Calculate rows
+    for (int row = 0; row < height; row++)
+    {
+
+        //  Calculate and print left white spaces
+        for (int white_spaces = height - 1; white_spaces > row; white_spaces--)
+        {
+            printf(" ");
+        }
+
+        //Calculate and print left hashes
+        for (int left_hashes = 0; left_hashes <= row; left_hashes++)
+        {
+            printf("#");
+        }
+
+        //Print gap
+        for (int gap = 0; gap < 2; gap++)
+        {
+            printf(" ");
+        }
+
+        // Calculate and print right hashes
+        for (int right_col = 0; right_col <= row; right_col++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
